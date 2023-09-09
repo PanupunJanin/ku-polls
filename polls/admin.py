151e9a8b541153c61b26accc0f350346c521a372
PanupunJanin <panupan.ja@gmail.com>
2023-09-09 14:30:53 +0700

    unpush all files Revert "update admin page"
    This reverts commit 656129f817832ce26f14221e209b17fe79235c1f.

--- polls/admin.py
+++ polls/admin.py
@@ -1,21 +1,4 @@
 from django.contrib import admin
-from .models import Question, Choice
+from .models import Question
 
-
-class ChoiceInlines(admin.TabularInline):
-    model = Choice
-    extra = 1
-
-
-class AdminQuestion(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['question_text']}),
-        ('Data information', {'fields': ['pub_date', 'end_date']}),
-    ]
-    inlines = [ChoiceInlines]
-    list_display = ('question_text', 'pub_date', 'end_date')
-    list_filter = ['pub_date', 'end_date']
-    search_fields = ['question_text']
-
-
-admin.site.register(Question, AdminQuestion)
+admin.site.register(Question)
