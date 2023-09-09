0cd205b71113a56b4b9303e7dc75c0a8dd5cc8f2
PanupunJanin <panupan.ja@gmail.com>
2023-09-09 14:38:15 +0700

    Revert "add end date and default publish date with is_published and can_vote method"
    This reverts commit 2fef7e7045e04c89f6bbb5e167b827497bf8fced.

--- polls/models.py
+++ polls/models.py
@@ -5,13 +5,10 @@ from django.contrib import admin
 
 
 class Question(models.Model):
-    """Question model with publish date and end date(optional)"""
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published', default=timezone.localtime, blank=True)
-    end_date = models.DateTimeField('date ended', null=True, blank=True)
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        """Return question text description string of each question."""
         return self.question_text
 
     @admin.display(
@@ -19,29 +16,14 @@ class Question(models.Model):
         ordering="pub_date",
     )
     def was_published_recently(self):
-        """Return if the question is published recently(1 day)"""
-        now = timezone.localtime()
+        now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
-    def is_published(self):
-        """Return if the question is published or not."""
-        now = timezone.localtime()
-        return self.pub_date <= now
-
-    def can_vote(self):
-        """Return if voting is allowed or not."""
-        now = timezone.localtime()
-        if self.end_date is None:
-            return self.pub_date <= now
-        return self.pub_date <= now <= self.end_date
 
 
 class Choice(models.Model):
-    """Choice model for each question"""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        """Return choice text description string of each choice."""
         return self.choice_text
