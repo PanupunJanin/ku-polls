69d92783cc19d0f8b60096e0036256a89c8cadde
PanupunJanin <panupan.ja@gmail.com>
2023-09-09 14:27:56 +0700

    add admin display

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -13,6 +14,10 @@ class Question(models.Model):
         """Return question text description string of each question."""
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+    )
     def was_published_recently(self):
         """Return if the question is published recently(1 day)"""
         now = timezone.localtime()
